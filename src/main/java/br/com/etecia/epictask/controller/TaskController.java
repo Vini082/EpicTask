03aed484f4dd273495d5e70254e88752db8b5b03
Vini082 <viniciussantos082@gmail.com>
2025-09-03 22:18:46 -0300

    criando o BD

--- src/main/java/br/com/etecia/epictask/controller/TaskController.java
+++ src/main/java/br/com/etecia/epictask/controller/TaskController.java
@@ -3,6 +3,7 @@ package br.com.etecia.epictask.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +14,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.etecia.epictask.model.Task;
+import br.com.etecia.epictask.repository.TaskRepository;
 
 
 @Controller
 @RequestMapping("/tasks")
 public class TaskController {
 
-private List<Task> repository = new ArrayList<>();
+//private List<Task> repository = new ArrayList<>();
+
+@Autowired
+private TaskRepository repository;
 
 
 @GetMapping
     
 public String listTasks(Model model){
- 
- model.addAttribute("tasks",repository);
-
+model.addAttribute("tasks",repository.findAll());
 return "tasks";
 
 }
@@ -40,7 +43,7 @@ public String ShowForm(){
 @PostMapping("/form")
 public String create(Task task, RedirectAttributes redirect){
    System.out.println("Cadastrando tarefas..." + task);
-   repository.add(task);
+   repository.save(task);
    redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso");
    return "redirect:/tasks";
 }
