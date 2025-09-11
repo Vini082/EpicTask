f8e9636a5f356fa6b4b2cdc7fa199d57e28db2d8
Vini082 <viniciussantos082@gmail.com>
2025-09-10 22:13:37 -0300

    validação

--- src/main/java/br/com/etecia/epictask/controller/TaskController.java
+++ src/main/java/br/com/etecia/epictask/controller/TaskController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.etecia.epictask.model.Task;
 import br.com.etecia.epictask.repository.TaskRepository;
+import jakarta.validation.Valid;
 
 
 @Controller
@@ -36,12 +38,14 @@ return "tasks";
 }
 
 @GetMapping("/form")
-public String ShowForm(){
+public String ShowForm(Task task){
    return "form";
 }
 
 @PostMapping("/form")
-public String create(Task task, RedirectAttributes redirect){
+public String create(@Valid Task task, BindingResult result, RedirectAttributes redirect){
+   if(result.hasErrors()) return "form";
+
    System.out.println("Cadastrando tarefas..." + task);
    repository.save(task);
    redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso");
